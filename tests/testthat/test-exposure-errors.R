a23b646df928dbb4e8c9c2199d975c510cfd59a8
Will Beasley <wibeasley@hotmail.com>
2017-02-21 13:42:29 -0600

    starting exposure smoke test

--- tests/testthat/test-exposure-errors.R
+++ tests/testthat/test-exposure-errors.R
@@ -2,8 +2,8 @@ library(testthat)
 context("Exposure Errors")
 
 test_that("exposure - inherit error - data.frame", {
-  d <- data.frame(id=seq_along(letters), names=letters, stringsAsFactors=FALSE) #These aren't really dirty.  And should have no conversion problems
+  #The dataset by itself is fine.  The problem is that it doesn't inherit from tibble.
+  d <- data.frame(id=seq_along(letters), names=letters, stringsAsFactors=FALSE)
 
   expect_error(exposure(d), "The `d` parameter must inherit from `tibble::tbl`.", fixed=TRUE)
-
 })
---
+++ tests/testthat/test-exposure.R
@@ -0,0 +1,26 @@
+library(testthat)
+context("Exposure")
+
+test_that("smoke-test", {
+  ds <- tibble::tribble(
+    ~client_id, ~referral_id, ~referral_date, ~removal_begin_date,
+            1L,           1L,   "2016-01-01",       NA_character_,
+            1L,           2L,   "2015-01-01",        "2015-02-01",
+            1L,           3L,   "2014-01-01",       NA_character_,
+            1L,           3L,   "2014-01-01",        "2015-01-01",
+            2L,           4L,   "2015-01-01",        "2015-03-01",
+            2L,           5L,   "2014-01-01",        "2014-02-01",
+            3L,           6L,   "2015-01-01",       NA_character_,
+            3L,           7L,   "2014-01-01",       NA_character_,
+            4L,           8L,   "2014-01-01",       NA_character_,
+            5L,           9L,   "2014-01-01",        "2015-01-01"
+  ) %>%
+  dplyr::mutate(
+    referral_date         = as.Date(referral_date),
+    removal_begin_date    = as.Date(removal_begin_date)
+  )
+
+
+  d_returned <- exposure(ds)
+  expect_true(!is.null(d_returned))
+})
