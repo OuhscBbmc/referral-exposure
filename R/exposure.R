95594512590c84e7dd2a90178d2e02f858f88a56
Will Beasley <wibeasley@hotmail.com>
2017-02-21 13:19:42 -0600

    starting `exposure()`

---
+++ R/exposure.R
@@ -0,0 +1,11 @@
+#' The main function of the package.
+#'
+#' Calculating exposure over multiple referrals
+#'
+#' @param d tibble.  See Details below. Required
+#' @export
+#' @md
+#'
+exposure <- function( d ) {
+  return( d )
+}
