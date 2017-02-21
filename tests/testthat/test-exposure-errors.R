96d7fccf22fe40bb099dcbcdf5e37639f4afe1c1
Will Beasley <wibeasley@hotmail.com>
2017-02-21 14:26:10 -0600

    tidy

--- tests/testthat/test-exposure-errors.R
+++ tests/testthat/test-exposure-errors.R
@@ -4,16 +4,16 @@ context("Exposure Errors")
 ds_plain <-
   tibble::tribble(
     ~client_id, ~referral_id, ~referral_date, ~removal_begin_date,
-    1L,           1L,   "2016-01-01",       NA_character_,
-    1L,           2L,   "2015-01-01",        "2015-02-01",
-    1L,           3L,   "2014-01-01",       NA_character_,
-    1L,           3L,   "2014-01-01",        "2015-01-01",
-    2L,           4L,   "2015-01-01",        "2015-03-01",
-    2L,           5L,   "2014-01-01",        "2014-02-01",
-    3L,           6L,   "2015-01-01",       NA_character_,
-    3L,           7L,   "2014-01-01",       NA_character_,
-    4L,           8L,   "2014-01-01",       NA_character_,
-    5L,           9L,   "2014-01-01",        "2015-01-01"
+            1L,           1L,   "2016-01-01",       NA_character_,
+            1L,           2L,   "2015-01-01",        "2015-02-01",
+            1L,           3L,   "2014-01-01",       NA_character_,
+            1L,           3L,   "2014-01-01",        "2015-01-01",
+            2L,           4L,   "2015-01-01",        "2015-03-01",
+            2L,           5L,   "2014-01-01",        "2014-02-01",
+            3L,           6L,   "2015-01-01",       NA_character_,
+            3L,           7L,   "2014-01-01",       NA_character_,
+            4L,           8L,   "2014-01-01",       NA_character_,
+            5L,           9L,   "2014-01-01",        "2015-01-01"
   ) %>%
   dplyr::mutate(
     client_id             = as.character(client_id),
--- tests/testthat/test-exposure.R
+++ tests/testthat/test-exposure.R
@@ -1,19 +1,19 @@
 library(testthat)
 context("Exposure")
 
-ds_plain <-
+ds_kid_referral_input <-
   tibble::tribble(
     ~client_id, ~referral_id, ~referral_date, ~removal_begin_date,
-    1L,           1L,   "2016-01-01",       NA_character_,
-    1L,           2L,   "2015-01-01",        "2015-02-01",
-    1L,           3L,   "2014-01-01",       NA_character_,
-    1L,           3L,   "2014-01-01",        "2015-01-01",
-    2L,           4L,   "2015-01-01",        "2015-03-01",
-    2L,           5L,   "2014-01-01",        "2014-02-01",
-    3L,           6L,   "2015-01-01",       NA_character_,
-    3L,           7L,   "2014-01-01",       NA_character_,
-    4L,           8L,   "2014-01-01",       NA_character_,
-    5L,           9L,   "2014-01-01",        "2015-01-01"
+            1L,           1L,   "2016-01-01",       NA_character_,
+            1L,           2L,   "2015-01-01",        "2015-02-01",
+            1L,           3L,   "2014-01-01",       NA_character_,
+            1L,           3L,   "2014-01-01",        "2015-01-01",
+            2L,           4L,   "2015-01-01",        "2015-03-01",
+            2L,           5L,   "2014-01-01",        "2014-02-01",
+            3L,           6L,   "2015-01-01",       NA_character_,
+            3L,           7L,   "2014-01-01",       NA_character_,
+            4L,           8L,   "2014-01-01",       NA_character_,
+            5L,           9L,   "2014-01-01",        "2015-01-01"
   ) %>%
   dplyr::mutate(
     client_id             = as.character(client_id),
@@ -24,8 +24,8 @@ ds_plain <-
 
 
 test_that("smoke-test", {
-  d_returned <- exposure(ds_plain)
+  d_returned <- exposure(ds_kid_referral_input)
   expect_true(!is.null(d_returned))
 })
 
-rm(ds_plain)
+rm(ds_kid_referral_input)
