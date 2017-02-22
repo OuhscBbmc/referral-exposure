787e9f12fbb16ec01855147bfdcbc2350a72ade2
Will Beasley <wibeasley@hotmail.com>
2017-02-22 10:34:38 -0600

    disable function 3; catch error of function 1

--- tests/testthat/test-failing-functions.R
+++ tests/testthat/test-failing-functions.R
@@ -6,12 +6,17 @@ test_that("Failing-1", {
   expect_true(!is.null(d_returned))
 })
 
+test_that("Failing-1-caught", {
+  expected_message <- "Vector 1 has type 'character' not 'integer'"
+  expect_error(failing_1(), expected_message, fixed=TRUE)
+})
+
 test_that("Passing-2", {
   d_returned <- passing_2()
   expect_true(!is.null(d_returned))
 })
 
-test_that("Failing-3", {
-  d_returned <- failing_3()
-  expect_true(!is.null(d_returned))
-})
+# test_that("Failing-3", {
+#   d_returned <- failing_3()
+#   expect_true(!is.null(d_returned))
+# })
